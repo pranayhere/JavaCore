e9fffa72c24ebf2c8dff28c13f9d23bb35c51ede
pranay <pranaybsankpal2050@gmail.com>
2020-04-15 17:11:18 +0530

    space optimised

--- src/com/pranay/Leetcode30days/ProductOfArrayExceptSelf.java
+++ src/com/pranay/Leetcode30days/ProductOfArrayExceptSelf.java
@@ -6,7 +6,9 @@ public class ProductOfArrayExceptSelf {
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4};
 		int[] result = productExceptSelf(nums);
-		System.out.println(Arrays.toString(result));
+		int[] res = productExceptSelfSpaceOptimised(nums);
+//		System.out.println(Arrays.toString(result));
+		System.out.println(Arrays.toString(res));
 	}
 
 	private static int[] productExceptSelf(int[] nums) {
@@ -17,13 +19,13 @@ public class ProductOfArrayExceptSelf {
 		for (int i = 1; i<nums.length; i++) {
 			fwd[i] = fwd[i-1] * nums[i];
 		}
-		System.out.println(Arrays.toString(fwd));
+//		System.out.println(Arrays.toString(fwd));
 
 		bwd[bwd.length - 1] = nums[nums.length - 1];
 		for (int i = nums.length - 2; i >=0; i--) {
 			bwd[i] = bwd[i+1] * nums[i];
 		}
-		System.out.println(Arrays.toString(bwd));
+//		System.out.println(Arrays.toString(bwd));
 
 		for (int i = 0; i < nums.length; i++) {
 			if (i == 0) {
@@ -35,5 +37,21 @@ public class ProductOfArrayExceptSelf {
 			}
 		}
 		return res;
+	}
+
+	private static int[] productExceptSelfSpaceOptimised(int[] nums) {
+		int[] ans = new int[nums.length];
+		ans[0] = 1;
+		for (int i = 1; i<nums.length; i++) {
+			ans[i] = nums[i-1] * ans[i-1];
+		}
+		System.out.println(Arrays.toString(ans));
+		System.out.println(Arrays.toString(nums));
+		int R = 1;
+		for (int i = nums.length - 1; i>=0; i--) {
+			ans[i] = ans[i] * R;
+			R = R * nums[i];
+		}
+		return ans;
 	}
 }
