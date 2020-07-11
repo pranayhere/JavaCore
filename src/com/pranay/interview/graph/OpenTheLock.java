c2af04307225049e2b1376edc18e905113634ada
pranay <pranaybsankpal2050@gmail.com>
2020-07-12 00:01:35 +0530

    variable renaming

--- src/com/pranay/interview/graph/OpenTheLock.java
+++ src/com/pranay/interview/graph/OpenTheLock.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class OpenTheLock {
     public static void main(String[] args) {
-        String[] deadends = {"0201","0101","0102","1212","2002"};
+        String[] deadends = {"0201", "0101", "0102", "1212", "2002"};
         String target = "0202";
 
 //        String[] deadends = {"8888"};[]\poui[[]=
@@ -31,17 +31,17 @@ public class OpenTheLock {
         int depth = 0;
         while (!q.isEmpty()) {
             int size = q.size();
-            for (int i = 0; i < size; i++) {
+            for (int n = 0; n < size; n++) {
                 String node = q.poll();
 
                 if (node.equals(target)) {
                     return depth;
                 }
 
-                for (int n = 0; n < 4; n++) {
+                for (int i = 0; i < 4; i++) {
                     for (int d = -1; d <= 1; d += 2) {
-                        int y = ((node.charAt(n) - '0') + d + 10) % 10;
-                        String neighbour = node.substring(0, n) + ("" + y) + node.substring(n+1);
+                        int y = ((node.charAt(i) - '0') + d + 10) % 10;
+                        String neighbour = node.substring(0, i) + ("" + y) + node.substring(i + 1);
                         System.out.println("Neighbour : " + neighbour);
 
                         if (!seen.contains(neighbour) && !deadends.contains(neighbour)) {
